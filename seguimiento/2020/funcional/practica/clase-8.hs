5f438c85aea6ba611dfdd1a86d03bfb146939e70
Gaston Prieto <gastonprietoutn@gmail.com>
2020-05-30 17:18:49 -0300

    funcional - codigo clase 8

--- seguimiento/2020/funcional/practica/clase-8.hs
+++ seguimiento/2020/funcional/practica/clase-8.hs
@@ -1,1 +1,8 @@
 import Text.Show.Functions()
+
+sumateTres a b c = a + b + c
+--sumateTres a = (\b -> (\c -> a + b + c))
+
+sumateDos (a, b) = a + b
+
+filtradosYMapeados xs = [2 * x | x <- xs, even x, x > 6]
